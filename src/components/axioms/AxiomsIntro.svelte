f366aae673cfc2b62626bcfba8f526995b26b242
slyght-hp <c.kendallwc@gmail.com>
2025-02-22 00:32:24 -0600

    Create AxiomsIntro.svelte

---
+++ src/components/axioms/AxiomsIntro.svelte
@@ -0,0 +1,23 @@
+<script type="ts">
+  import Typewriter from 'svelte-typewriter';
+</script>
+
+<Typewriter cascade interval={20} >  <p class="text-center pb-4"> This is a copy of Alphonse's "Axioms for Agents." </p>
+
+  <p class="text-center"> I have no idea whether they actually came from Alphonse. </p>
+
+  <p class="text-center"> Writing all this down doesn't seem like the old man's style... </p>
+
+  <p class="text-center"> Maybe it started with him, but it's been making the rounds a long time. </p>
+
+  <p class="text-center"> Who knows how many of us have added to it over the years? </p>
+
+  <ul class="flex flex-col items-center pb-4">
+  <li class="text-center w-full">It all comes back to the <span class="text-delta-green">same old mission</span>: </p>
+  <li class="self-start"Find the threat</li>
+  <li class="self-start"Stop the threat</li>
+  <li class="self-start"Cover it up so it's like the threat never happened</li>
+
+  <p class="text-center"> Keep these rules close. They might just save your life. </p>
+  
+</Typewriter>
