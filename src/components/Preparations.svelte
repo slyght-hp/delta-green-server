ad0f21121f5a27f6588e734594eac01058f7ae05
slyght-hp <c.kendallwc@gmail.com>
2025-02-22 00:21:49 -0600

    Create Preparations.svelte

---
+++ src/components/Preparations.svelte
@@ -0,0 +1,18 @@
+<script type="ts">
+  import Typewriter from 'svelte-typewriter';
+</script>
+
+<Typewriter cascade interval={20} >
+  <br />
+  <p class="text-left"> You're not going in unequipped. </p>
+  <p class="text-left"> In locker XX at the XX bus station in XX, California, you will find a burner phone. </p>
+  <p class="text-left"> A number has been pre-programmed. </p>
+  <p class="text-left"> Use this, and only this, to contact your handler, Agent XX. </p>
+  <p class="text-left"> You will also find your cover identity, which shows you to be a member of an FBI Task Force. </p>
+  <p class="text-left"> The cover will hold up at a glance, but don't give anyone a reason to dig deeper into your identity. </p>
+  <p class="text-left"> There are three Beretta 9mm handguns with two clips of ammunition apiece. </p>
+  <p class="text-left"> Should you need anything else, contact Agent XX. </p>
+  <p class="text-left"> Keep Agent XX informed of your progress. </p>
+  <p class="text-left pb-4"> Destroy the phone when the operation is complete. </p>
+  
+</Typewriter>
