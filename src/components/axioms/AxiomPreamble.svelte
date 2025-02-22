8832f0a7515595bf2203f76d8d152d0175b21069
slyght-hp <c.kendallwc@gmail.com>
2025-02-21 23:48:08 -0600

    Update AxiomPreamble.svelte

--- src/components/axioms/AxiomPreamble.svelte
+++ src/components/axioms/AxiomPreamble.svelte
@@ -4,25 +4,24 @@
 </script>
 
 <Typewriter cascade on:done={onFinish}>
-  <p class="text-center pb-4"> Here are those <span class="text-delta-green">"Rogers' Rules of Ranging"</span> we talked about. </p>
+  <p class="text-center pb-4"> Something is wrong in Hellbend, California. </p>
 
-  <p class="text-center"> I have no idea whether they actually came from <span class="text-delta-green">Alphonse</span>.</p>
+  <p class="text-center"> Two civilians have been murdered. </p>
 
-  <p class="text-center"> Writing all this down doesn't seem like the old man's style..</p>
+  <p class="text-center"> Their bodies were torn apart. </p>
 
-  <p class="text-center"> Maybe it started with him, but it's been making the rounds a long time.</p>
+  <p class="text-center pb-4"> It's up to you to find out why. </p>
 
-  <p class="text-center">Who knows how many of us have added to it over the years?</p>
-
-  <p class="text-center"> If you share it with any FNGs, be sure to tell them not to mistake it for intelligence tradecraft. </p>
-
-  <p class="text-center pb-4"> It's tradecraft for <span class="text-delta-green">Delta Green</span> agents on the ground.</p>
-  
-  <ul class="flex flex-col justify-center items-center pb-4">
-    <p class="text-delta-green">It all comes back to the same old mission:</p>
-    <li> Find the threat </li>
-    <li> Stop the threat </li>
-    <li> Cover it up so it's like the threat never happened </li>
+  <ul class="flex flex-col items-center pb-4">
+    <li class="text-center w-full">Agent, your mission is as follows: </li>
+    <li class="self-start">1) Determine who or what is killing the people of Hellbend.</li>
+    <li class="self-start">2) If the threat is unnatural, isolate it and destroy it.</li>
+    <li class="self-start">3) Close the case so law enforcement doesn't look any closer.</li>
+    <li class="self-start">4) 4) Leave no witnesses to any unnatural events.</li>
   </ul>
+	
+  <p class="text-center"> Time is of the essence. </p>
+
+  <p class="text-center"> You will find further details in the attached files. </p>
 
 </Typewriter>
