25d8a1f6b913503a99d6ce9c6ccf6a05de86238b
slyght-hp <c.kendallwc@gmail.com>
2025-02-22 01:01:47 -0600

    Update Agents.ts

--- src/Agents.ts
+++ src/Agents.ts
@@ -1,7 +1,7 @@
 export const Agents: Array<{user: string, password: string}> = [
-  {user: 'Jay', password: '01011995'},
+  {user: 'Jay', password: '05061995'},
   {user: 'Andrew', password: '07021991'},
   {user: 'Taylor', password: '02231997'},
-  {user: 'program', password: 'outlaws'}
+  {user: 'a', password: 'z'}
 ]
 export default Agents;
