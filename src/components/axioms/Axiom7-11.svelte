419281e261acd5f09812707743120ec0340a2b72
slyght-hp <c.kendallwc@gmail.com>
2025-02-23 13:59:32 -0600

    Update and rename AxiomElevenThroughTwenty.svelte to Axiom7-11.svelte

---
+++ src/components/axioms/Axiom7-11.svelte
@@ -0,0 +1,37 @@
+<script type="ts">
+  import Typewriter from 'svelte-typewriter';
+</script>
+
+<Typewriter cascade interval={20}>
+  <br />
+  <ol>
+    <li>
+      <p><span class="text-delta-green">7. Always lie to anyone who isn't a member of Delta Green.</span></p>
+      <p class="pl-6">  About everything. </p>
+      <p class="pl-6">  Always. </p>
+      <p class="pl-6 mb-4">  And make sure your lies are specific and consistent. </p>
+    </li>
+    <li>
+      <p><span class="text-delta-green">8. Friendlies can know the truth about an individual operation.</span></p>
+      <p class="pl-6 mb-4">  But they can never know the truth about Delta Green. </p>
+    </li>
+    <li>
+      <p><span class="text-delta-green">9. Always scout your destination, no matter how secure you think it is.</span></p>
+      <p class="pl-6">  Never bring the whole team to do surveillance. </p>
+      <p class="pl-6 mb-4">  Perfect paranoia is perfect awareness. </p>
+    </li>
+    <li>
+      <p><span class="text-delta-green">10. Never do anything alone.</span></p>
+      <p class="pl-6">  Always have someone watch your back. </p>
+      <p class="pl-6">  When you are sleeping, pissing, eating, searching a location, interviewing a witness, </p>
+      <p class="pl-6 mb-4">  or when your nose is buried in research, have someone watching your six. </p>
+    </li>
+    <li>
+      <p><span class="text-delta-green">11. Always establish a rally point in case the team has to scatter and regroup.</span></p>
+      <p class="pl-6">  The rally point should be tactically advantageous in the event that not </p>
+      <p class="pl-6">  everyone manages to shake pursuers and the opportunity arises to ambush them.</p>
+      <p class="pl-6">  The rally point should be pre-stocked with anything you need to escape </p>
+      <p class="pl-6 mb-4">  the opposition: money, travel documents, a spare vehicle if you can manage it.</p>
+    </li>
+  </ol>
+</Typewriter>
