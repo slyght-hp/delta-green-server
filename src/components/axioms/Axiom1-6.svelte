3b7ad94214dea7ccf5a0c379acda137dba428f86
slyght-hp <c.kendallwc@gmail.com>
2025-02-23 13:58:51 -0600

    Update and rename AxiomOneThroughTen.svelte to Axiom1-6.svelte

--- src/components/axioms/Axiom1-6.svelte
+++ src/components/axioms/Axiom1-6.svelte
@@ -33,26 +33,5 @@
       <p class="pl-6">  About anything. </p>
       <p class="pl-6 mb-4">  Ever. </p>
     </li>
-    <li>
-      <p><span class="text-delta-green">7. Always lie to anyone who isn't a member of Delta Green.</span></p>
-      <p class="pl-6">  About everything. </p>
-      <p class="pl-6">  Always. </p>
-      <p class="pl-6 mb-4">  And make sure your lies are specific and consistent. </p>
-    </li>
-    <li>
-      <p><span class="text-delta-green">8. Friendlies can know the truth about an individual operation.</span></p>
-      <p class="pl-6 mb-4">  But they can never know the truth about Delta Green. </p>
-    </li>
-    <li>
-      <p><span class="text-delta-green">9. Always scout your destination, no matter how secure you think it is.</span></p>
-      <p class="pl-6">  Never bring the whole team to do surveillance. </p>
-      <p class="pl-6 mb-4">  Perfect paranoia is perfect awareness. </p>
-    </li>
-    <li>
-      <p><span class="text-delta-green">10. Never do anything alone.</span></p>
-      <p class="pl-6">  Always have someone watch your back. </p>
-      <p class="pl-6">  When you are sleeping, pissing, eating, searching a location, interviewing a witness, </p>
-      <p class="pl-6 mb-4">  or when your nose is buried in research, have someone watching your six. </p>
-    </li>
   </ol>
 </Typewriter>
