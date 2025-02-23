4ded4dc2fd46300464ce88ac52b90e3bc88a43a6
slyght-hp <c.kendallwc@gmail.com>
2025-02-23 14:00:25 -0600

    Update and rename AxiomTwentyOneThroughThirty.svelte to Axiom12-18.svelte

---
+++ src/components/axioms/Axiom12-18.svelte
@@ -0,0 +1,41 @@
+<script type="ts">
+  import Typewriter from 'svelte-typewriter';
+</script>
+
+<Typewriter cascade interval={20}>
+  <br />
+  <ol>
+    <li>
+      <p><span class="text-delta-green">12. If you're attacked, break contact.</span></p>
+      <p class="pl-6">  Do not stand and fight when the opposition has the initiative. </p>
+      <p class="pl-6 mb-4">  Retreat, regroup at the rally point, go back later and kill them in their sleep. </p>
+    </li>
+    <li>
+      <p><span class="text-delta-green">13. Never take a chance if you don't have to.</span></p>
+      <p class="pl-6 mb-4">  If it feels wrong, it is wrong </p>
+    </li>
+    <li>
+      <p><span class="text-delta-green">14. Don't wait until you see the whites of their eyes.</span></p>
+      <p class="pl-6">  If firearms have no effect, it's best to find that out </p>
+      <p class="pl-6 mb-4">  while you still have the time and the space to break contact. </p>
+    </li>
+    <li>
+      <p><span class="text-delta-green">15. Give no "fair warnings."</span></p>
+      <p class="pl-6 mb-4">  Surprise is the only advantage you have. </p>
+    </li>
+    <li>
+      <p><span class="text-delta-green">16. Taking live prisoners usually carries more operational hazards than benefits.</span></p>
+      <p class="pl-6 mb-4">  If you do take prisoners, never leave them together to cook up a story between them. </p>
+    </li>
+    <li>
+      <p><span class="text-delta-green">17. Torture rarely works on our brand of opposition.</span></p>
+      <p class="pl-6">  Look for laptops, notepads, cell phones, documents, and journals. </p>
+      <p class="pl-6 mb-4">  Passwords and encryption are often broken more easily and more reliably than men.</p>
+    </li>
+    <li>
+      <p><span class="text-delta-green">18. Assume nothing.</span></p>
+      <p class="pl-6">  Don't believe what you are told. </p>
+      <p class="pl-6 mb-4">  Double-check it yourself. </p>
+    </li>
+  </ol>
+</Typewriter>
