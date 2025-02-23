0f7ac6a299bde4f999c2352f1d6bb348730724d2
slyght-hp <c.kendallwc@gmail.com>
2025-02-23 03:10:52 -0600

    Update and rename AxiomSixThroughTen.svelte to AxiomEleventThroughTwenty.svelte

---
+++ src/components/axioms/AxiomEleventThroughTwenty.svelte
@@ -0,0 +1,49 @@
+<script type="ts">
+  import Typewriter from 'svelte-typewriter';
+</script>
+
+<Typewriter cascade interval={20}>
+  <br />
+  <ol>
+    <li>
+     <p><span class="text-delta-green">11. Always establish a rally point in case the team has to scatter and regroup.</span></p>
+      <p class="pl-6">  The rally point should be tactically advantageous in the event that not </p>
+      <p class="pl-6">  everyone manages to shake pursuers and the opportunity arises to ambush them.</p>
+      <p class="pl-6">  The rally point should be pre-stocked with anything you need to escape </p>
+      <p class="pl-6 mb-4">  the opposition: money, travel documents, a spare vehicle if you can manage it.</p>
+    </li>
+      <p><span class="text-delta-green">12. If you're attacked, break contact.</span></p>
+      <p class="pl-6">  Do not stand and fight when the opposition has the initiative. </p>
+      <p class="pl-6 mb-4">  Retreat, regroup at the rally point, go back later and kill them in their sleep. </p>
+    </li>
+      <p><span class="text-delta-green">13. Never take a chance if you don't have to.</span></p>
+      <p class="pl-6 mb-4">  If it feels wrong, it is wrong </p>
+    </li>
+      <p><span class="text-delta-green">14. Don't wait until you see the whites of their eyes.</span></p>
+      <p class="pl-6">  If firearms have no effect, it's best to find that out </p>
+      <p class="pl-6 mb-4">  while you still have the time and the space to break contact. </p>
+    </li>
+      <p><span class="text-delta-green">15. Give no "fair warnings."</span></p>
+      <p class="pl-6 mb-4">  Surprise is the only advantage you have. </p>
+    </li>
+      <p><span class="text-delta-green">16. Taking live prisoners usually carries more operational hazards than benefits.</span></p>
+      <p class="pl-6 mb-4">  If you do take prisoners, never leave them together to cook up a story between them. </p>
+    </li>
+      <p><span class="text-delta-green">17. Torture rarely works on our brand of opposition.</span></p>
+      <p class="pl-6">  Look for laptops, notepads, cell phones, documents, and journals. </p>
+      <p class="pl-6 mb-4">  Passwords and encryption are often broken more easily and more reliably than men.</p>
+    </li>
+      <p><span class="text-delta-green">18. Assume nothing.</span></p>
+      <p class="pl-6">  Don't believe what you are told. </p>
+      <p class="pl-6 mb-4">  Double-check it yourself. </p>
+    </li>
+      <p><span class="text-delta-green">19. Everyone is always potentially under control by the opposition.</span></p>
+      <p class="pl-6 mb-4">  Including your teammates. </p>
+    </li>
+      <p><span class="text-delta-green">20. Time your moment.</span></p>
+      <p class="pl-6">  Save your energy, limit your exposure, strike when you are ready. </p>
+      <p class="pl-6">  Be utterly thorough. </p>
+      <p class="pl-6 mb-4">  Be utterly ruthless. </p>
+    </li>
+  </ol>
+</Typewriter>
