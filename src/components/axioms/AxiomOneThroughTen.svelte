78e08a18239b50a215c0f74e02a28b4b6dc05aa8
slyght-hp <c.kendallwc@gmail.com>
2025-02-23 03:22:18 -0600

    Update AxiomOneThroughTen.svelte

--- src/components/axioms/AxiomOneThroughTen.svelte
+++ src/components/axioms/AxiomOneThroughTen.svelte
@@ -5,41 +5,50 @@
 <Typewriter cascade interval={20} >
   <br />
   <ol>
-    <p><span class="text-delta-green">1.The first commandment is, Thou shall not get caught.</span></p>
+    <li>
+      <p><span class="text-delta-green">1.The first commandment is, Thou shall not get caught.</span></p>
       <p class="pl-6">  You do not have a "get out of jail free" card. You do not have a license to kill. </p>
       <p class="pl-6">  To the world outside Delta Green, you are a criminal, a terrorist and a traitor.</p>
       <p class="pl-6">  If you are arrested, you will keep quiet and say nothing.</p>
     </li>
-      <p><span class="text-delta-green">2. Always have your go-bag packed and ready for an operation</span> (Or, as agents have taking to calling it, a Night at the Opera).</p>
+    <li>
+      <p><span class="text-delta-green">2. Always have your go-bag packed and ready for an operation</span> <em>(Or, as agents have taking to calling it, a Night at the Opera).</em></p>
       <p class="pl-6 mb-4">  Your go-bag should include sanitized gear, weapons, cash, clothing </p>
-    </li>
+    <li>
       <p><span class="text-delta-green">3. Stick to your cover names.</span></p>
       <p class="pl-6">  The less you learn about each other during an operation, the safer you all are. </p>
       <p class="pl-6 mb-4"> You can't betray what you don't know. </p>
     </li>
+    <li>
       <p><span class="text-delta-green">4. Always keep to your cover identity.</span></p>
       <p class="pl-6 mb-4">  Your cover should always be someone with a reason for asking the questions you need answered. </p>
     </li>
+    <li>
       <p><span class="text-delta-green">5. Covers last only as long as they are necessary.</span></p>
       <p class="pl-6">  Even the best covers can't stand up to intense scrutiny. </p>
       <p class="pl-6 mb-4">  Make them, use them, ditch them. </p>
     </li>
+    <li>
       <p><span class="text-delta-green">6. Other than sticking to your cover, never lie to a fellow member of Delta Green.</span></p>
       <p class="pl-6">  About anything. </p>
       <p class="pl-6 mb-4">  Ever. </p>
     </li>
+    <li>
       <p><span class="text-delta-green">7. Always lie to anyone who isn't a member of Delta Green.</span></p>
       <p class="pl-6">  About everything. </p>
       <p class="pl-6">  Always. </p>
       <p class="pl-6 mb-4">  And make sure your lies are specific and consistent. </p>
     </li>
+    <li>
       <p><span class="text-delta-green">8. Friendlies can know the truth about an individual operation.</span></p>
       <p class="pl-6 mb-4">  But they can never know the truth about Delta Green. </p>
     </li>
+    <li>
       <p><span class="text-delta-green">9. Always scout your destination, no matter how secure you think it is.</span></p>
       <p class="pl-6">  Never bring the whole team to do surveillance. </p>
       <p class="pl-6 mb-4">  Perfect paranoia is perfect awareness. </p>
     </li>
+    <li>
       <p><span class="text-delta-green">10. Never do anything alone.</span></p>
       <p class="pl-6">  Always have someone watch your back. </p>
       <p class="pl-6">  When you are sleeping, pissing, eating, searching a location, interviewing a witness, </p>
