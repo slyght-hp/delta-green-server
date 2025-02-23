da3cca63b70146960a54a4277cccf02d0668b0c5
slyght-hp <c.kendallwc@gmail.com>
2025-02-23 02:12:11 -0600

    Update Preparations.svelte

--- src/components/Preparations.svelte
+++ src/components/Preparations.svelte
@@ -11,6 +11,7 @@
   <p class="text-left"> 2) Three (3) cover IDs, showing each of you as a member of an FBI Task Force. </p>
   <p class="text-left pb-4"> These covers will hold up at a glance, but don't give anyone a reason to dig deeper into your identity. </p>
   <p class="text-left pb-4"> 3) Three (3) Beretta 9mm handguns with two (2) clips of ammunition apiece. </p>
+  <p class="text-left pb-4"> 4) Two thousand (2,000) dollars in cash. </p>
   <p class="text-left"> Should you need anything else, contact Agent Giden. </p>
   <p class="text-left pb-4"> Keep Agent Gideon informed of your progress. </p>
   <p class="text-left pb-4"> When the operation is complete, destroy the burner phone and IDs, and return the weapons to the storage facility. </p>
