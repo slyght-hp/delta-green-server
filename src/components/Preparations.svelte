eaf00c8cacdcfa7c257ee37db638ddebebca92cc
slyght-hp <c.kendallwc@gmail.com>
2025-02-23 13:45:51 -0600

    Update Preparations.svelte

--- src/components/Preparations.svelte
+++ src/components/Preparations.svelte
@@ -4,16 +4,14 @@
 
 <Typewriter cascade interval={20} >
 <br />
-  <p class="text-left pb-4"> You're not going in unprepared. </p>
-  <p class="text-left"> At the Olive Drive Self Storage facility in Bakersfield, CA, you will find your requisitioned equipment. </p>
+  <p class="text-left pb-4"> At the Olive Drive Self Storage facility in Bakersfield, CA, you will find your requisitioned equipment. </p>
   <p class="text-left"> 1) One (1) burner phone, with a single phone number on it. </p>
   <p class="text-left pb-4"> Use this, and only this, to contact your handler, Agent Gideon. </p>
   <p class="text-left"> 2) Three (3) cover IDs, showing each of you as a member of an FBI Task Force. </p>
   <p class="text-left pb-4"> These covers will hold up at a glance, but don't give anyone a reason to dig deeper into your identity. </p>
   <p class="text-left pb-4"> 3) Three (3) Beretta 9mm handguns with two (2) clips of ammunition apiece. </p>
-  <p class="text-left pb-4"> 4) Two thousand (2,000) dollars in cash. </p>
-  <p class="text-left"> Should you need anything else, contact Agent Giden. </p>
-  <p class="text-left pb-4"> Keep Agent Gideon informed of your progress. </p>
+  <p class="text-left pb-4"> 4) Two thousand ($2,000) dollars in cash. </p>
+  <p class="text-left pb-4"> Keep Agent Gideon informed of your progress, and contact them should you need anything. </p>
   <p class="text-left pb-4"> When the operation is complete, destroy the burner phone and IDs, and return the weapons to the storage facility. </p>
   
 </Typewriter>
